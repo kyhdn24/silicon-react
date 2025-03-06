0a609ce85cc2336ef7ab91da84e4e94acda380a6
Hans Mattin-Lassei <hans.mattin-lassei@epndata.com>
2025-03-06 10:01:01 +0100

    FAQ API

--- src/components/elements/Accordion.jsx
+++ src/components/elements/Accordion.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import AccordionItem from './AccordionItem'
+import { FaqContext } from '../../contexts/FaqContext'
 
 const Accordion = () => {
+    const { faqs } = useContext(FaqContext)
+
     return (
         <div class="accordion" id="faq-accordion">
 
-            <AccordionItem parent="faq-accordion" item="" />      
+            {
+                faqs.map(faq => (<AccordionItem key={faq.id} parent="faq-accordion" item={faq} /> ))
+            }     
 
         </div>
     )
--- src/components/elements/AccordionItem.jsx
+++ src/components/elements/AccordionItem.jsx
@@ -4,11 +4,11 @@ const AccordionItem = ({parent, item}) => {
   return (
     <div class="accordion-item">
         <h2 class="accordion-header">
-            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target={`#faq-${item.id}`} aria-expanded="true" aria-controls={`faq-${item.id}`}>
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target={`#faq-${item.id}`} aria-expanded="false" aria-controls={`faq-${item.id}`}>
                 {item.title}
             </button>
         </h2>
-        <div id={`faq-${item.id}`} class="accordion-collapse collapse show" data-bs-parent={`#${parent}`}>
+        <div id={`faq-${item.id}`} class="accordion-collapse collapse" data-bs-parent={`#${parent}`}>
             <div class="accordion-body">
                 {item.content}
             </div>
